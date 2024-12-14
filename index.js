6c918fd01f113e712eb882add0e1ed4c77108921
Tien Kim <tienkim9920@gmail.com>
2024-12-14 16:12:26 +0700

    update index

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ const PORT = process.env.PORT || 8000;
 const cors = require("cors");
 app.use(cors());
 
-app.set('view engine', 'ejs');
-app.set('views', './views');
-
 var bodyParser = require("body-parser");
 const BaseResponse = require("./base.response");
 
@@ -32,10 +29,6 @@ let accounts = [
         role: 'USER'
     }
 ];
-
-app.get('/', (req, res) => {
-    res.render('index');
-})
 
 app.get('/blogs', (req, res) => {
     try {
